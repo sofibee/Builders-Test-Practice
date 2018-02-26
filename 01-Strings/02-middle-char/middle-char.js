33059ef21099942de7e69988dd18bfdbffa16922
Scott D'Alessandro <sdaless22@gmail.com>
2018-02-26 11:24:55 -0500

    Update middle-char.js

--- 01-Strings/02-middle-char/middle-char.js
+++ 01-Strings/02-middle-char/middle-char.js
@@ -1,8 +1,1 @@
-function getMid(s) {
-  //Code goes here!
 
-  if (s.length % 2 !== 0) {
-    return s[Math.floor(s.length / 2)];
-  }
-  return s[Math.floor(s.length / 2) - 1] + s[Math.floor(s.length / 2)];
-}
